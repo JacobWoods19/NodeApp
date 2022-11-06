1b51deaf13c45f9c62ddffb853fae00916558ab8
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2022-11-06 06:40:09 -0600

    fsafj

--- public/js/callAPI.js
+++ public/js/callAPI.js
@@ -4,21 +4,6 @@
 // ["ca55c8b2-2494-4341-a878-8c7ea508a4b0","01/02/03","snow","18.42901","14.124124","test@gmail.com"]]
 
 
-
-// Call average responsons time api
-function averageResponseTime() {
-    fetch('https://clownfish-app-y6vt9.ondigitalocean.app/get_average_response')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        document.getElementById("averageResponseTime").innerHTML = data;
-    })
-    .catch(err => {
-        console.log(err);
-    });
-}
-
-
 function deleteIncident(incident_id) {
     var payload = {
         id : incident_id
@@ -81,8 +66,107 @@ const getIncidents = async (alert_type) => {
 
     }
     // add each long and lat to the map
-
-
 }
+const getAverageResponseTime = async () => {
+    const response = await fetch('https://clownfish-app-y6vt9.ondigitalocean.app/get_average_response');
+    const myJson = await response.json();
+    console.log(myJson);
+    //response in days
+    // {
+    //     "average": 7127.0
+    // }
+    let average = myJson.average;
+    //set to document element id avg_response_time 
+    document.getElementById("avg_response_time").innerHTML = average + " days";
+}
+//Populate chart with api data of response times
+//Example response:     
+//returns array of response times for each resolved report
+// [ {date, days to respond}, {date, days to respond}, ...]
+const getResponseTimes = async () => {
+    const response = await fetch('https://clownfish-app-y6vt9.ondigitalocean.app/get_chart_data');
+    const myJson = await response.json();
+    //populate my chart with the data from the api
+    //build the chart data
+    let labels = [];
+    let data = [];
+    for (let i = 0; i < myJson.length; i++) {
+        labels.push(myJson[i].date);
+        data.push(myJson[i].response_time);
+    }
+    console.log("Data:")
+    console.log(data);
+    console.log("Labels:")
+    console.log(labels);
+    const data_dict = {
+        labels: labels,
+        datasets: [{
+          label: 'Response Time Over Time',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: data,
+        }]
+      };
+    const config = {
+        type: 'line',
+        data: data_dict,
+        options: {}
+    };
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+}
+//Build radar chart based on the data from the api
+//Example response:
+// {[category1, category2, category3], [avg_response_time1, avg_response_time2, avg_response_time3]}
+const getRadarData = async () => {
+    const response = await fetch('https://clownfish-app-y6vt9.ondigitalocean.app/avg_response_time_by_category');
+    const myJson = await response.json();
+    console.log(myJson);
+    //build the chart data
+    let labels = [];
+    let data = [];
+    for (let i = 0; i < myJson[0].length; i++) {
+        labels.push(myJson[0][i]);
+        data.push(myJson[1][i]);
+    }
+    console.log("Data:")
+    console.log(data);
+    console.log("Labels:")
+    console.log(labels);
+    const data_dict = {
+        labels: labels,
+        datasets: [{
+            label: 'Average Response Time by Category',
+            data: data,
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+            borderColor: 'rgba(255, 255, 255, 1)',
+            borderWidth: 1
+        }]
+    };
+    const config = {
+        type: 'doughnut',
+        data: data_dict,
+        options: {
+        }
+    };
+    var myChart = new Chart(
+        document.getElementById('radarChart'),
+        config
+    );
+}
+
+    
+    
+
+
+getAverageResponseTime();
 getIncidents();
-averageResponseTime();
+getResponseTimes();
+getRadarData();
+
